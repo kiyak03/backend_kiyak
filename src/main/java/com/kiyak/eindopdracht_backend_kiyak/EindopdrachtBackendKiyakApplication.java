b8e7ca2356fc8d69792b675f043d07075ea226ac
fatih <f.kiyak@outlook.com>
2021-02-11 00:49:31 +0100

    updated!

---
+++ src/main/java/com/kiyak/eindopdracht_backend_kiyak/EindopdrachtBackendKiyakApplication.java
@@ -0,0 +1,14 @@
+package com.kiyak.eindopdracht_backend_kiyak;
+
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendKiyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindopdrachtBackendKiyakApplication.class, args);
+	}
+
+}
