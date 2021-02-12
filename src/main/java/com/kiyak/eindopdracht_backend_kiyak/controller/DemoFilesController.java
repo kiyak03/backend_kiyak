ccaa77605ec4ac6c2ff41c52439d329ec2c1abc8
fatih <f.kiyak@outlook.com>
2021-02-12 17:07:52 +0100

    updated!

---
+++ src/main/java/com/kiyak/eindopdracht_backend_kiyak/controller/DemoFilesController.java
@@ -0,0 +1,72 @@
+package com.kiyak.eindopdracht_backend_kiyak.controller;
+
+
+import com.kiyak.eindopdracht_backend_kiyak.domain.DemoFiles;
+import com.kiyak.eindopdracht_backend_kiyak.payload.response.FileResponse;
+import com.kiyak.eindopdracht_backend_kiyak.payload.response.MessageResponse;
+import com.kiyak.eindopdracht_backend_kiyak.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Controller
+@CrossOrigin("http://localhost:8081")
+public class DemoFilesController {
+
+
+    @Autowired
+    private FileStorageService storageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<MessageResponse> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            storageService.store(file);
+
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageResponse(message));
+        }
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<List<FileResponse>> getListFiles() {
+        List<FileResponse> files = storageService.getAllFiles().map(demoFile -> {
+            String fileDownloadUri = ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/files/")
+                    .path(demoFile.getId())
+                    .toUriString();
+
+            return new FileResponse(
+                    demoFile.getName(),
+                    fileDownloadUri,
+                    demoFile.getType(),
+                    demoFile.getData().length);
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(files);
+    }
+
+    @GetMapping("/files/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable String id) {
+        DemoFiles demoFiles = storageService.getFile(id);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + demoFiles.getName() + "\"")
+                .body(demoFiles.getData());
+    }
+
+}
